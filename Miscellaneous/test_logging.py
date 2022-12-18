2639ce4ab2dfaf3769c35771ed8b8a89eda7a5d1
nahidniloy14 <nahidniloysqa@gmail.com>
2022-12-18 12:52:19 +0600

    folders are organized

--- Miscellaneous/test_logging.py
+++ Miscellaneous/test_logging.py
@@ -3,7 +3,7 @@ import logging
 def test_logging():
     # loggerName=inspect.stack()[1][3]
     logger=logging.getLogger(__name__) #method to log everything#__name__ prints name
-    fileHandler=logging.FileHandler('logfile.log')
+    fileHandler=logging.FileHandler('../Reports/logfile.log')
     formatter=logging.Formatter("%(asctime)s :%(levelname)s :%(name)s :%(message)s ")
     fileHandler.setFormatter(formatter)
     logger.addHandler(fileHandler) #accept filehandler object #logger format and file
